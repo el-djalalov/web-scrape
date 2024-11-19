e0bf74ef071d3cbf6434a337333c8844a162ed96
Elyor Djalalov <edjalalov@hotmail.com>
2024-11-19 15:59:37 +0500

    Deletable edges added

--- app/workflow/_conponents/edges/DeletableEdge.tsx
+++ app/workflow/_conponents/edges/DeletableEdge.tsx
@@ -1,13 +1,46 @@
 "use client";
 
-import { BaseEdge, EdgeProps, getSmoothStepPath } from "@xyflow/react";
+import { Button } from "@/components/ui/button";
+import {
+	BaseEdge,
+	EdgeLabelRenderer,
+	EdgeProps,
+	getSmoothStepPath,
+	useReactFlow,
+} from "@xyflow/react";
 import React from "react";
 
 function DeletableEdge(props: EdgeProps) {
-	const [edgePath] = getSmoothStepPath(props);
+	const [edgePath, labelX, labelY] = getSmoothStepPath(props);
+	const { setEdges } = useReactFlow();
+
 	return (
 		<>
-			<BaseEdge path={edgePath} markerEnd={props.markerEnd} />
+			<BaseEdge
+				path={edgePath}
+				markerEnd={props.markerEnd}
+				style={props.style}
+			/>
+			<EdgeLabelRenderer>
+				<div
+					style={{
+						position: "absolute",
+						transform: `translate(-50%, 50%) translate(${labelX}px, ${labelY}px )`,
+						pointerEvents: "all",
+					}}
+				>
+					<Button
+						variant={"destructive"}
+						size={"sm"}
+						className="border w-6 h-6 cursor-pointer rounded-full text-xs leading-none hover:shadow-lg"
+						onClick={() => {
+							setEdges(edges => edges.filter(edge => edge.id !== props.id));
+						}}
+					>
+						X
+					</Button>
+				</div>
+			</EdgeLabelRenderer>
 		</>
 	);
 }
--- app/workflow/_conponents/topbar/Topbar.tsx
+++ app/workflow/_conponents/topbar/Topbar.tsx
@@ -21,7 +21,6 @@ function Topbar({ title, subtitle, workflowId }: Props) {
 		<header className="flex px-4 border-b-2 border-separate justify-between w-full h-[60px] sticky top-0 bg-background z-10">
 			<div className="flex justify-between w-full items-center">
 				<div className="flex items-center justify-center gap-2">
-					<TooltipWrapper content="Go back workflows">
 						<Button
 							variant={"secondary"}
 							size={"sm"}
@@ -30,7 +29,6 @@ function Topbar({ title, subtitle, workflowId }: Props) {
 							<ChevronLeftIcon size={20} />
 							Back
 						</Button>
-					</TooltipWrapper>
 					<div className="flex justify-center items-center gap-4">
 						<p className="font-bold text-ellipsis truncate">{title}</p>
 						<Separator orientation="vertical" />
