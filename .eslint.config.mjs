a19c09f87734db695c2a8ea4c9c9fd462b143d52
Elyor Djalalov <edjalalov@hotmail.com>
2025-05-30 15:40:33 +0500

    updates on execute functions and eslint

---
+++ .eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "eslint-define-config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default defineConfig([
+  js.configs.recommended, // It's good practice to include ESLint's own recommendations
+  ...compat.extends("next/core-web-vitals"),
+
+  {
+    rules: {
+      // Your custom rules
+    },
+  },
+]);
--- app/api/workflows/cron/route.tsx
+++ app/api/workflows/cron/route.tsx
@@ -1,44 +1,92 @@
-import { getAppUrl } from "@/lib/helper/appUrl";
 import prisma from "@/lib/prisma";
-import { WorkflowStatus } from "@/types/workflow";
+import { ExecuteWorkflow } from "@/lib/workflow/executeWorkflow";
+import { TaskRegistry } from "@/lib/workflow/task/registry";
+import {
+  WorkflowStatus,
+  WorkflowExecutionStatus,
+  WorkflowExecutionTrigger,
+  WorkflowExecutionPlan,
+  ExecutionPhaseStatus,
+} from "@/types/workflow";
+import parser from "cron-parser";
 
 export async function GET(request: Request) {
-	const now = new Date();
+  const now = new Date();
 
-	const workflows = await prisma.workflow.findMany({
-		select: { id: true },
-		where: {
-			status: WorkflowStatus.PUBLISHED,
-			cron: { not: null },
-			nextRunAt: { lte: now },
-		},
-	});
+  const dueWorkflows = await prisma.workflow.findMany({
+    select: {
+      id: true,
+      cron: true,
+      userId: true,
+      defination: true,
+      executionPlan: true,
+    },
+    where: {
+      status: WorkflowStatus.PUBLISHED,
+      cron: { not: null },
+      nextRunAt: { lte: now },
+    },
+  });
 
-	for (const workflow of workflows) {
-		triggerWorkflow(workflow.id);
-	}
+  let executionsStarted = 0;
 
-	return Response.json({ workflowsToRun: workflows.length }, { status: 200 });
-}
+  for (const wf of dueWorkflows) {
+    try {
+      if (!wf.executionPlan || !wf.cron || !wf.userId) {
+        `Workflow ${wf.id} is missing necessary data (executionPlan, cron, or userId). Skipping.`;
+        continue;
+      }
+      const executionPlan = JSON.parse(
+        wf.executionPlan
+      ) as WorkflowExecutionPlan;
+      const cronExpression = parser.parseExpression(wf.cron, { utc: true });
+      const nextRunForThisWorkflow = cronExpression.next().toDate();
 
-function triggerWorkflow(workflowId: string) {
-	const triggerApiUrl = getAppUrl(
-		`api/workflows/execute?workflowId=${workflowId}`
-	);
+      const execution = await prisma.workflowExecution.create({
+        data: {
+          workflowId: wf.id,
+          userId: wf.userId,
+          definition: wf.defination,
+          status: WorkflowExecutionStatus.PENDING,
+          startedAt: new Date(),
+          trigger: WorkflowExecutionTrigger.CRON,
+          phases: {
+            create: executionPlan.flatMap((phase) => {
+              return phase.nodes.flatMap((node) => {
+                if (!TaskRegistry[node.data.type]) {
+                  console.warn(
+                    `Task type ${node.data.type} not found in registry. Skipping node execution.`
+                  );
+                  return [];
+                }
+                return {
+                  userId: wf.userId!,
+                  status: ExecutionPhaseStatus.CREATED,
+                  number: phase.phase,
+                  node: JSON.stringify(node),
+                  name: TaskRegistry[node.data.type].label,
+                };
+              });
+            }),
+          },
+        },
+      });
 
-	console.log("@@TRIGGER URL", triggerApiUrl);
+      ExecuteWorkflow(execution.id, nextRunForThisWorkflow);
+      executionsStarted++;
 
-	fetch(triggerApiUrl, {
-		headers: {
-			Authorization: `Bearer ${process.env.API_SECRET!}`,
-		},
-		cache: "no-store",
-	}).catch(err => {
-		console.error(
-			"Error triggering workflow with id",
-			workflowId,
-			": Error => ",
-			err
-		);
-	});
+      await prisma.workflow.update({
+        where: { id: wf.id },
+        data: { nextRunAt: nextRunForThisWorkflow },
+      });
+    } catch (error) {}
+  }
+
+  return Response.json(
+    {
+      message: `Processed ${dueWorkflows.length} due workflows. Started ${executionsStarted} executions.`,
+      workflowsToRunCount: dueWorkflows.length,
+    },
+    { status: 200 }
+  );
 }
